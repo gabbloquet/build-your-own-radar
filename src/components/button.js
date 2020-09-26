6549f50c796293635fe3bf12ef67fe84974f17ba
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-09-26 17:06:54 +0200

    chore: delete useless lines

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,6 @@
 export const buttonBuilder = (div, content, url) => {
   div.append('div:a')
     .attr('class', 'vtmn-btn vtmn-btn_variant--primary')
-    .attr('id', content.toLowerCase())
     .attr('href', url)
     .html(content)
-
-  // const button = document.getElementById(content.toLowerCase())
-  // button.addEventListener('click', action)
 }
